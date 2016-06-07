3d8b8a1aa75c0545da515d5b2286b441fc183320
Albin Sunnanbo <albin@sunnanbo.com>
2016-06-07 10:00:55 +0200

    Change title to ASP.NET-StatusBoard

--- StatusBoard.Demo/Properties/AssemblyInfo.cs
+++ StatusBoard.Demo/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StatusBoard.Demo")]
+[assembly: AssemblyTitle("ASP.NET-StatusBoard.Demo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("StatusBoard.Demo")]
