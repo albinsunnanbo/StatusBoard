b6bc9dc16896e92c0eaf27d17ccaa706da7e3f8a
Albin Sunnanbo <albin.sunnanbo@soprasteria.com>
2021-07-09 17:11:52 +0200

    Add sample checks demonstrating the timeout features

---
+++ StatusBoard.Owin.Demo/Checks/LongRunningCheckTimeoutError.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using StatusBoard.Core;
+using System.Threading.Tasks;
+
+namespace StatusBoard.Owin.Demo.Checks
+{
+    public class LongRunningCheckTimeoutError : Core.StatusCheck
+    {
+        public LongRunningCheckTimeoutError()
+        {
+            Timeout = TimeSpan.FromSeconds(1);
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "Always OK, after a while";
+            }
+        }
+
+        public async override Task<CheckResult> GetCurrentStatus()
+        {
+            await Task.Delay(2500);
+            return CheckResult.ResultOk("Worth waiting for?");
+        }
+    }
+}
---
+++ StatusBoard.Owin.Demo/Checks/LongRunningCheckTimeoutWarning.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using StatusBoard.Core;
+using System.Threading.Tasks;
+
+namespace StatusBoard.Owin.Demo.Checks
+{
+    public class LongRunningCheckTimeoutWarning : Core.StatusCheck
+    {
+        public LongRunningCheckTimeoutWarning()
+        {
+            Timeout = TimeSpan.FromSeconds(1);
+            TimeoutErrorLevel = StatusValue.WARNING;
+        }
+        public override string Name
+        {
+            get
+            {
+                return "Always OK, after a while";
+            }
+        }
+
+        public async override Task<CheckResult> GetCurrentStatus()
+        {
+            await Task.Delay(2500);
+            return CheckResult.ResultOk("Worth waiting for?");
+        }
+    }
+}
