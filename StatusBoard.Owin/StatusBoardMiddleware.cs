6a202199a0ddb7239abc327de83e1f8cb6420bca
Albin Sunnanbo <albin.sunnanbo@soprasteria.com>
2021-07-13 10:41:09 +0200

    Refactor: Rewrite using Uri.Segments instead of PathString.StartsWithSegments

--- StatusBoard.Owin/StatusBoardMiddleware.cs
+++ StatusBoard.Owin/StatusBoardMiddleware.cs
@@ -19,101 +19,82 @@ namespace StatusBoard.Owin
 
         public async override Task Invoke(IOwinContext context)
         {
-            PathString remainingLevel1;
-            if (context.Request.Path.StartsWithSegments(new PathString("/Status"), out remainingLevel1))
+            var segments = context.Request.Uri.Segments;
+            if (segments.Length >= 2 && segments[1].ToLower().TrimEnd('/') == "status")
             {
-                var webResponse = await CreateWebResponse(remainingLevel1);
+                var webResponse = await CreateWebResponse(segments);
                 if (webResponse != null)
                 {
                     await context.WriteToOwinContext(webResponse);
                     return;
                 }
-                if (remainingLevel1.Value == "/")
+                if (segments[1].ToLower().EndsWith("/"))
                 {
                     context.Response.Redirect(context.Request.Path.Value.Substring(0, context.Request.Path.Value.Length - 1));
                     return;
                 }
-                await context.Response.WriteAsync($"Invalid status request '{remainingLevel1.Value}'");
+                await context.Response.WriteAsync($"Invalid status request '{segments[1]}'");
                 return;
             }
             await Next.Invoke(context);
         }
 
-        public async Task<WebResponse> CreateWebResponse(PathString remainingLevel1)
+        public async Task<WebResponse> CreateWebResponse(string[] segments)
         {
-            PathString remainingLevel2;
-            PathString remainingLevel3;
-
-            if (!remainingLevel1.HasValue)
+            WebResponse webResponse;
+            if (segments.Length == 2)
             {
-                var webResponse = options.GetStartPage();
+                webResponse = options.GetStartPage();
                 return webResponse;
             }
-            if (remainingLevel1.StartsWithSegments(new PathString("/js"), out remainingLevel2))
+            switch (segments[2].ToLower().TrimEnd('/'))
             {
-                return options.GetJs();
-            }
-            if (remainingLevel1.StartsWithSegments(new PathString("/css"), out remainingLevel2))
-            {
-                return options.GetCss();
-            }
-            if (remainingLevel1.StartsWithSegments(new PathString("/jQuery"), out remainingLevel2))
-            {
-                return (options.GetJquery());
-            }
-            if (remainingLevel1.StartsWithSegments(new PathString("/Directory"), out remainingLevel2))
-            {
-                var webResponse = options.GetDirectoryListing();
-                return webResponse;
-            }
-            if (remainingLevel1.StartsWithSegments(new PathString("/Proxy"), out remainingLevel2))
-            {
-                if (remainingLevel2.HasValue)
-                {
-                    var nextSlash = remainingLevel2.Value.IndexOf('/', 1);
-                    var proxyId = int.Parse(remainingLevel2.Value.Substring(1, nextSlash - 1));
-                    if (remainingLevel2.StartsWithSegments(new PathString("/" + proxyId), out remainingLevel3))
+                case "js":
+                    return options.GetJs();
+                case "css":
+                    return options.GetCss();
+                case "jquery":
+                    return options.GetJquery();
+                case "directory":
+                    webResponse = options.GetDirectoryListing();
+                    return webResponse;
+                case "proxy":
+                    if (segments.Length > 3)
                     {
+                        var proxyCheckId = segments[3].TrimEnd('/');
+                        var proxyId = int.Parse(proxyCheckId);
                         var proxyBaseUrl = options.GetProxyBaseUri(proxyId).AbsoluteUri.TrimEnd('/');
-                        var proxyCombinedUrl = proxyBaseUrl + remainingLevel3.Value;
+                        var proxyCombinedUrl = proxyBaseUrl + "/" + string.Join("/", segments.Skip(4).Select(s => s.TrimEnd('/')));
                         using (var wc = new System.Net.WebClient())
                         {
                             var result = await wc.DownloadStringTaskAsync(proxyCombinedUrl);
                             return WebResponse.JsonResponse(result);
                         }
                     }
-                }
-                else
-                {
-                    var webResponse = options.GetProxyListing();
+                    else
+                    {
+                        webResponse = options.GetProxyListing();
+                        return webResponse;
+                    }
+                case "check":
+                    var checkId = segments[3].Trim('/');
+                    webResponse = await options.RunCheck(checkId, options.CheckIndividualTimeout);
                     return webResponse;
-                }
-            }
-            if (remainingLevel1.StartsWithSegments(new PathString("/Check"), out remainingLevel2))
-            {
-                var checkId = remainingLevel2.Value.TrimStart('/');
-                var webResponse = await options.RunCheck(checkId, options.CheckIndividualTimeout);
-                return webResponse;
-            }
-            if (remainingLevel1.StartsWithSegments(new PathString("/CheckAllNoProxy"), out remainingLevel2))
-            {
-                var webResponse = await options.RunAllChecks(checkProxies: false, timeout: options.CheckAllNoProxyTimeout);
-                return webResponse;
-            }
-            if (remainingLevel1.StartsWithSegments(new PathString("/CheckAll"), out remainingLevel2))
-            {
-                var webResponse = await options.RunAllChecks(timeout: options.CheckAllTimeout);
-                return webResponse;
-            }
-            if (remainingLevel1.StartsWithSegments(new PathString("/CheckAllFailOnWarning"), out remainingLevel2))
-            {
-                var webResponse = await options.RunAllChecks(StatusValue.WARNING, timeout: options.CheckAllFailOnWarningTimeout);
-                return webResponse;
-            }
-            if (remainingLevel1.StartsWithSegments(new PathString("/CheckAllFailOnError"), out remainingLevel2))
-            {
-                var webResponse = await options.RunAllChecks(StatusValue.ERROR, timeout: options.CheckAllFailOnErrorTimeout);
-                return webResponse;
+                case "checkallnoproxy":
+                    webResponse = await options.RunAllChecks(checkProxies: false, timeout: options.CheckAllNoProxyTimeout);
+                    return webResponse;
+                case "checkall":
+                    webResponse = await options.RunAllChecks(timeout: options.CheckAllTimeout);
+                    return webResponse;
+
+                case "checkallfailonwarning":
+
+                    webResponse = await options.RunAllChecks(StatusValue.WARNING, timeout: options.CheckAllFailOnWarningTimeout);
+                    return webResponse;
+                case "checkallfailonerror":
+
+                    webResponse = await options.RunAllChecks(StatusValue.ERROR, timeout: options.CheckAllFailOnErrorTimeout);
+                    return webResponse;
             }
             return null;
         }
